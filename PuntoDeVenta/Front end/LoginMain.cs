b9cb38a420243499c61f1b19257124713c43f115
Caleb Zacarias Garcia <112665042+elcalebItsur@users.noreply.github.com>
2024-11-11 18:31:18 -0600

    Login Nuevo

---
+++ PuntoDeVenta/Front end/LoginMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoDeVenta.Front_end
+{
+    public partial class LoginMain : Form
+    {
+        public LoginMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
--- PuntoDeVenta/Program.cs
+++ PuntoDeVenta/Program.cs
@@ -17,7 +17,7 @@ namespace PuntoDeVenta
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new login());
+            Application.Run(new LoginMain());
         }
     }
 }

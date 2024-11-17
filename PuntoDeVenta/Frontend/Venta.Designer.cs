edbfa012b32a0bd45424f55d1e977455bcea24c7
Caleb Zacarias Garcia <112665042+elcalebItsur@users.noreply.github.com>
2024-11-16 20:34:40 -0600

    actualización ventas

--- PuntoDeVenta/Frontend/Venta.Designer.cs
+++ PuntoDeVenta/Frontend/Venta.Designer.cs
@@ -34,11 +34,13 @@
             this.btnEliminarProducto = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnRealizarVenta = new System.Windows.Forms.Button();
-            this.cmbCategoria = new System.Windows.Forms.ComboBox();
             this.btnGuardar = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.txtCodigo_Barras = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -46,12 +48,14 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(170)))), ((int)(((byte)(222)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.btnEliminarProducto);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btnRealizarVenta);
-            this.panel1.Controls.Add(this.cmbCategoria);
             this.panel1.Controls.Add(this.btnGuardar);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.txtCodigo_Barras);
@@ -73,7 +77,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(30, 311);
+            this.textBox1.Location = new System.Drawing.Point(30, 412);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(188, 32);
             this.textBox1.TabIndex = 16;
@@ -82,7 +86,7 @@
             // 
             this.btnEliminarProducto.BackColor = System.Drawing.Color.Red;
             this.btnEliminarProducto.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEliminarProducto.Location = new System.Drawing.Point(242, 310);
+            this.btnEliminarProducto.Location = new System.Drawing.Point(242, 411);
             this.btnEliminarProducto.Name = "btnEliminarProducto";
             this.btnEliminarProducto.Size = new System.Drawing.Size(95, 32);
             this.btnEliminarProducto.TabIndex = 15;
@@ -93,7 +97,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(26, 276);
+            this.label1.Location = new System.Drawing.Point(26, 377);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(273, 20);
             this.label1.TabIndex = 14;
@@ -103,7 +107,7 @@
             // 
             this.btnRealizarVenta.BackColor = System.Drawing.Color.Lime;
             this.btnRealizarVenta.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRealizarVenta.Location = new System.Drawing.Point(85, 563);
+            this.btnRealizarVenta.Location = new System.Drawing.Point(85, 554);
             this.btnRealizarVenta.Name = "btnRealizarVenta";
             this.btnRealizarVenta.Size = new System.Drawing.Size(190, 65);
             this.btnRealizarVenta.TabIndex = 13;
@@ -111,19 +115,11 @@
             this.btnRealizarVenta.UseVisualStyleBackColor = false;
             this.btnRealizarVenta.Click += new System.EventHandler(this.btnRealizarVenta_Click_1);
             // 
-            // cmbCategoria
-            // 
-            this.cmbCategoria.FormattingEnabled = true;
-            this.cmbCategoria.Location = new System.Drawing.Point(30, 460);
-            this.cmbCategoria.Name = "cmbCategoria";
-            this.cmbCategoria.Size = new System.Drawing.Size(188, 24);
-            this.cmbCategoria.TabIndex = 9;
-            // 
             // btnGuardar
             // 
             this.btnGuardar.BackColor = System.Drawing.Color.Lime;
             this.btnGuardar.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGuardar.Location = new System.Drawing.Point(242, 149);
+            this.btnGuardar.Location = new System.Drawing.Point(242, 266);
             this.btnGuardar.Name = "btnGuardar";
             this.btnGuardar.Size = new System.Drawing.Size(95, 32);
             this.btnGuardar.TabIndex = 12;
@@ -133,17 +129,17 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(26, 435);
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(26, 496);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(71, 22);
+            this.label5.Size = new System.Drawing.Size(74, 21);
             this.label5.TabIndex = 4;
-            this.label5.Text = "Cajero";
+            this.label5.Text = "Cajero:";
             // 
             // txtCodigo_Barras
             // 
             this.txtCodigo_Barras.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCodigo_Barras.Location = new System.Drawing.Point(30, 148);
+            this.txtCodigo_Barras.Location = new System.Drawing.Point(30, 196);
             this.txtCodigo_Barras.Name = "txtCodigo_Barras";
             this.txtCodigo_Barras.Size = new System.Drawing.Size(188, 32);
             this.txtCodigo_Barras.TabIndex = 8;
@@ -152,11 +148,39 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(26, 113);
+            this.label8.Location = new System.Drawing.Point(141, 113);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(311, 23);
+            this.label8.Size = new System.Drawing.Size(77, 23);
             this.label8.TabIndex = 0;
-            this.label8.Text = "Código de barras del Producto";
+            this.label8.Text = "Ventas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 19);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Cantidad del producto:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(30, 266);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(188, 32);
+            this.textBox2.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(26, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 19);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Código de barras:";
             // 
             // Venta
             // 
@@ -178,7 +202,6 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.ComboBox cmbCategoria;
         private System.Windows.Forms.TextBox txtCodigo_Barras;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label8;
@@ -188,5 +211,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnEliminarProducto;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
